c9b1b1fa1f160c25c67d1cbfcb1ad4bc20d3695a
databorodata <nboldyrev90@gmail.com>
2024-02-11 20:55:09 +0100

    remove trash

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import BackgroundTasks, FastAPI
+from fastapi import FastAPI
 
 from app.database import create_db_and_tables
 from app.routers import router_dish, router_full_menu, router_menu, router_submenu
@@ -20,19 +20,10 @@ app = FastAPI(
 )
 
 
-@app.get('/menus/')
-async def get_menus(background_tasks: BackgroundTasks):
-    # Добавьте логику для вывода всех меню
-    # Используйте background_tasks.add_task() для асинхронного вызова задачи инвалидации кэша
-    pass
-
-
 @app.on_event('startup')
 async def on_startup() -> None:
     """Инициализирует базу данных при запуске приложения."""
     await create_db_and_tables()
-    # result = update_menu_from_sheet()
-    # print(f"ready? {result}")
 
 
 app.include_router(router_menu.router)
